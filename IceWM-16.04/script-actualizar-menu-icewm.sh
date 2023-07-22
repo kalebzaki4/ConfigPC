cda240206b67b2245e494a81e6de491d3ff7b06a
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-07-22 10:33:42 -0300

    iniciando o launcher

---
+++ IceWM-16.04/script-actualizar-menu-icewm.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+xdgmenumaker -i -f icewm > ~/.icewm/appmenu
+
+# Actualizar menu después que instales una aplicacion
+# porque si no haces esto no aparecen los nuevos iconos
+# en el menu de aplicaciones de icewm
+# Visto en "xdgmenumaker" https://github.com/gapan/xdgmenumaker
