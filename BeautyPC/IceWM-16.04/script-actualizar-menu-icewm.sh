4729625ab1b072e4b3e0782fbd80102c2c584563
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-22 16:47:41 -0300

    Projeto Completo

--- BeautyPC/IceWM-16.04/script-actualizar-menu-icewm.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-xdgmenumaker -i -f icewm > ~/.icewm/appmenu
-
-# Actualizar menu después que instales una aplicacion
-# porque si no haces esto no aparecen los nuevos iconos
-# en el menu de aplicaciones de icewm
-# Visto en "xdgmenumaker" https://github.com/gapan/xdgmenumaker
