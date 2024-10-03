d3c2e8d3b87a793329c54f3a02e39be15b78ecac
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-10-03 17:05:06 -0300

    Create Otimizar o Visual do windows - iGust.bat

---
+++ Projetos/Open-Source/BoostPc/Boost PC/PACK ESPECIAL/11 - Outros pack/iGust Pack/Pack/1- Batchs/Otimizar o Visual do windows - iGust.bat
@@ -0,0 +1,220 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a" & set "COL=%%b")
+Reg add HKCU\CONSOLE /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
+
+mode 96,30
+
+
+:variables
+set g=[92m
+set r=[91m
+set red=[04m
+set l=[1m
+set w=[0m
+set b=[94m
+set m=[95m
+set p=[35m
+set c=[35m
+set d=[96m
+set u=[0m
+set z=[91m
+set n=[96m
+set y=[40;33m
+set g2=[102m
+set r2=[101m
+set t=[40m
+set gg=[93m
+set q=[90m
+set gr=[32m
+set o=[38;5;202m
+set bb=[38;5;74m
+set nn=[38;5;82m
+set rr=[1;91m
+set blb=[1;94m
+set neon_vp=[1;38;5;129m
+set ha=[38;5;203m
+
+
+
+
+
+
+:memp
+title Otimizar visual do Windows - iGust
+cls
+FOR /F %%A in ('"PROMPT $H&FOR %%B in (1) DO REM"') DO SET "BS=%%A"
+echo.
+echo.
+echo.                       %m%██%n%╗    %m%██████%n%╗    %m%██%n%╗   %m%██%n%╗   %m%███████%n%╗   %m%████████%n%╗%m%    
+echo.                       %m%██%n%║   %m%██%n%╔════╝    %m%██%n%║   %m%██%n%║   %m%██%n%╔════╝   %n%╚══%m%██%n%╔══╝%m%    
+echo.                       %m%██%n%║   %m%██%n%║  %m%███%n%╗   %m%██%n%║   %m%██%n%║%m%   ███████%n%╗      %m%██%n%║       
+echo.                       %m%██%n%║   %m%██%n%║   %m%██%n%║   %m%██%n%║   %m%██%n%║   %n%╚════%m%██%n%║      %m%██%n%║       
+echo.                       %m%██%n%║   %n%╚%m%██████%n%╔╝   %n%╚%m%██████%n%╔╝   %m%███████%n%║      %m%██%n%║       
+echo.                       %n%╚═%n%╝    %n%╚═════╝     %n%╚═════╝    %n%╚══════╝      %n%╚═╝%m%  
+echo. %nn%    
+Call :TYPEfast "                             • Made By Memphis, Made For iGust "
+timeout /t 2 >NUL
+echo.
+echo.
+echo %r% "%q%───────────────────────────────────────────────────────────────────────────────────────────%r%" %w%
+echo.
+echo.
+call :ColorText 0 "         ["    
+call :ColorText 4 " 1"    
+call :ColorText 0 " ]"    
+call :ColorText 5 " O"    
+call :ColorText 5 "t"    
+call :ColorText 5 "i"       
+call :ColorText 5 "m" 
+call :ColorText 5 "i"    
+call :ColorText 5 "z"  
+call :ColorText 5 "a"     
+call :ColorText 5 "r"       
+call :ColorText 5 " o" 
+call :ColorText 5 " V"    
+call :ColorText 5 "i"       
+call :ColorText 5 "s" 
+call :ColorText 5 "u"    
+call :ColorText 5 "a"  
+call :ColorText 5 "l"     
+call :ColorText 5 " d"       
+call :ColorText 5 "o"       
+call :ColorText 5 " W"
+call :ColorText 5 "i"
+call :ColorText 5 "n"
+call :ColorText 5 "d"
+call :ColorText 5 "o"
+call :ColorText 5 "w"
+call :ColorText 5 "s"
+
+
+        call :ColorText 0 "                   ["    
+        call :ColorText 4        " 2"    
+        call :ColorText 0        " ]"    
+        call :ColorText f        " D" 
+        call :ColorText f         "i"       
+        call :ColorText f         "s"    
+        call :ColorText f         "c"    
+        call :ColorText f         "o" 
+        call :ColorText f         "r"    
+        call :ColorText f         "d"
+
+echo.
+echo.
+set /p opcao="> %b%"
+
+if %opcao% equ 1 goto op1
+if %opcao% equ 2 goto op2
+
+:teclaerradapae
+cls
+title Tecla errada!
+echo.
+echo.
+echo %r%                            Tecla errada, tome cuidado na proxima vez.
+timeout /t 5 >nul
+goto memp
+cls
+
+
+:op2
+cls
+title Discord - iGust
+start https://discord.gg/igust
+goto slk
+
+
+
+:op1
+cls
+title Otimizando o Visual do Windows - iGust
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DWM" /v "DWMWA_TRANSITIONS_FORCEDISABLED" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DWM" /v "DisallowAnimations" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d "0" /f
+Reg.exe add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MaxAnimate" /t REG_SZ /d "0" /f
+Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\VideoSettings" /v "VideoQualityOnBattery" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v "VisualFXSetting" /t REG_DWORD /d "3" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\AnimateMinMax" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ComboBoxAnimation" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ControlAnimations" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\CursorShadow" /v "DefaultApplied" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\DragFullWindows" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\DropShadow" /v "DefaultApplied" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\DWMAeroPeekEnabled" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\DWMEnabled" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\DWMSaveThumbnailEnabled" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\FontSmoothing" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ListBoxSmoothScrolling" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ListviewAlphaSelect" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ListviewShadow" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\MenuAnimation" /v "DefaultApplied" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\SelectionFade" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\TaskbarAnimations" /v "DefaultApplied" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\Themes" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\ThumbnailsOrIcon" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects\TooltipAnimation" /v "DefaultApplied" /t REG_DWORD /d "1" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "Theme" /t REG_DWORD /d "1" /f
+cls
+goto :slk
+
+
+
+
+
+
+
+
+:TYPE
+SET "d[text]=%~1"
+CALL :TYPELOOP
+GOTO :EOF
+
+
+:TYPELOOP
+IF "%d[text]:~0,1%" == "#" (
+    <NUL SET /P "=^!"
+) else (
+    <NUL SET /P "=d!BS!%d[text]:~0,1%"
+)
+SET "d[text]=%d[text]:~1%"
+IF "%d[text]%" == "" (
+    GOTO :EOF
+) else (
+    FOR /L %%J in (1, 100, 10000000) DO REM
+    GOTO :TYPELOOP
+)
+exit
+
+
+:TYPEFast
+SET "d[text]=%~1"
+CALL :TYPELOOPFast
+GOTO :EOF
+
+
+:TYPELOOPFast
+IF "%d[text]:~0,1%" == "#" (
+    <NUL SET /P "=^!"
+) else (
+    <NUL SET /P "=d!BS!%d[text]:~0,1%"
+)
+SET "d[text]=%d[text]:~1%"
+IF "%d[text]%" == "" (
+    GOTO :EOF
+) else (
+    FOR /L %%J in (1, 100, 1000000) DO REM
+    GOTO :TYPELOOPFast
+)
+
+
+:ColorText    
+echo off    
+<nul set /p ".=%DEL%" > "%~2"    
+findstr /v /a:%1 /R "^$" "%~2" nul    
+del "%~2" > nul 2>&1    
+goto :eof 
