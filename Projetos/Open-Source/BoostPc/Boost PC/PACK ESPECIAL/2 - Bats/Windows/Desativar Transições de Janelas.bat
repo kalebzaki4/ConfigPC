a539e9e6700c660988114f4802669a9e21153577
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-02-26 15:15:20 -0300

    Delete Desativar Transições de Janelas.bat

--- Projetos/Open-Source/BoostPc/Boost PC/PACK ESPECIAL/2 - Bats/Windows/Desativar Transições de Janelas.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Desabilitando transições de janelas...
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "UserPreferencesMask" /t REG_BINARY /d 90 12 03 80 /f
-echo Transições de janelas desabilitadas!
-pause
